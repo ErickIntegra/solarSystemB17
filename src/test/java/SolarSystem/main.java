1c27c84cde81d4638029f9bf3708a18bd63e33e0
ErickIntegra <steezy7jefe@gmail.com>
2023-11-02 17:25:49 -0400

    this commit has basic stucture of the solar system

---
+++ src/test/java/SolarSystem/main.java
@@ -0,0 +1,20 @@
+package SolarSystem;
+
+public class main {
+    public static void main(String[] args) {
+        solarSystemBasics solarsys=new solarSystemBasics();
+        solarsys.Sun="1";
+        solarsys.Moon="2";
+        solarsys.Planets="2";
+        solarsys.Stars="10";
+
+/*        *******Feature Sun*******
+        FeatureSun Sun=new FeatureSun();
+        Sun.name="panther";
+        Sun.color="black";
+        Sun.temp="-1223124kelvin";
+
+
+*/
+    }
+}
